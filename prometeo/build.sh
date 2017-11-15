fec3ce52b26e0587945cf9f0a440f4c43c49971f
gatblau <andres.alos.m@gmail.com>
2017-11-15 14:03:43 +0000

    Adds ssh keys to the docker build.

--- prometeo/build.sh
+++ prometeo/build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 mvn -f ../../prometeo/pom.xml package
 cp ../../prometeo/target/*.jar .
+if [ ! -f id_rsa ]; then
+    ssh-keygen -f id_rsa -N ''
+fi
 docker build -t "prometeo:0.0.1-0" .

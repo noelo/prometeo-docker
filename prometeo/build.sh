f2e7cfc460b33bb98591a048cff99b35683058f4
gatblau <andres.alos.m@gmail.com>
2017-11-17 18:00:10 +0000

    Updates SSH settings.

--- prometeo/build.sh
+++ prometeo/build.sh
@@ -4,4 +4,11 @@ cp ../../prometeo/target/*.jar .
 if [ ! -f id_rsa ]; then
     ssh-keygen -f id_rsa -N ''
 fi
+if [ ! -f config ]; then
+    cat <<EOF >> config
+Host *
+    StrictHostKeyChecking no
+    UserKnownHostsFile /dev/null
+EOF
+fi
 docker build -t "prometeo:0.0.1-0" .
